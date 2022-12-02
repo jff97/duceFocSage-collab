4e52b3b52f73a3e0c8a78009138a0d37afadce07
John Fox <107318784+jff97@users.noreply.github.com>
2022-12-02 11:05:26 -0600

    bleh

--- pythonStuff/recursion/recursiveModule.py
+++ pythonStuff/recursion/recursiveModule.py
@@ -14,16 +14,20 @@ class Fibonacci:
 #it sums the array recursively by adding the first element to the function call of the array without the first element
 class ArraySummer:
    def summ(self, arr):
-      if len(arr) == 1:
+      print(arr)
+      if len(arr) < 2:
          return arr[0]
       else:
-         m = (len(arr) - 1)//2
+         m = ((len(arr) - 1)//2)
          left_arr = self.copy_arr(arr, 0, m)
          right_arr = self.copy_arr(arr, m, (len(arr) - 1))
          return self.summ(left_arr) + self.summ(right_arr)
 
    def copy_arr(self, array, start, end):
       to_return = []
-      for i in range(start, end):
-         to_return.append(array[i])
+      if end == 0:
+         to_return.append(array[0])
+      else:
+         for i in range(start, end):
+            to_return.append(array[i])
       return to_return
--- pythonStuff/recursion/test.py
+++ pythonStuff/recursion/test.py
@@ -1,6 +1,13 @@
-l = [4, 5, 2, 234, 234, 54, 5, 7, 8]
-start = 3
-end = len(l) - 1
-print(l[end])
-for i in range(start, end, 1):
-   print(l[start + i])
+
+
+
+def copy_arr(array, start, end):
+      to_return = []
+      if end == 0:
+         to_return.append(array[0])
+      else:
+         for i in range(start, end):
+            to_return.append(array[i])
+      return to_return
+
+print(copy_arr([0, 1, 2, 3, 4, 5, 6, 7], 0, 0)[0])
